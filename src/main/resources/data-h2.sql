747491c49da77b7e90b66c32835d0bdefc635f0b
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-09-04 23:33:13 +0430

    📖 DOCS: Added default plain pass

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,5 +1,5 @@
-INSERT INTO USER("ID", "EMAIL", "FIRSTNAME", "LASTNAME", "PASSWORD")
-VALUES
+-- Default password for john.doe@gmail.com is: asd@123456
+INSERT INTO USER("ID", "EMAIL", "FIRSTNAME", "LASTNAME", "PASSWORD") VALUES
 (10001, 'john.doe@gmail.com', 'Jane', 'Doe', '$2a$10$Zj38WYKI.gGbhQWuATpY2.nI8CowD/ujyC7c2MozoyMBGNi8z6bui');
 
 INSERT INTO ROLE(id, name) VALUES(10001, 'ROLE_USER');
