f7460d47a927d2488012a93c2ddb239ec762c763
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-09-04 13:07:34 +0430

    🕯 START: Initiated the project

---
+++ src/main/java/com/glinboy/assignment/egs/EgsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.glinboy.assignment.egs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgsAssignmentApplication.class, args);
+	}
+
+}
