3d48ab28950283e767d7b45fa5f5fdcc20afe8bd
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-09-05 13:14:19 +0430

    📦 NEW: Added Update User block status to user repository

--- src/main/java/com/glinboy/assignment/egs/repository/UserRepository.java
+++ src/main/java/com/glinboy/assignment/egs/repository/UserRepository.java
@@ -3,12 +3,19 @@ package com.glinboy.assignment.egs.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.glinboy.assignment.egs.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByEmail(String email);
+	
 	Boolean existsByEmail(String email);
+	
+	@Modifying
+	@Query("UPDATE User u SET u.blocked = ?2 WHERE u.id = ?1")
+	void updateUserBlockStatus(Long id, Boolean status);
 
 }
