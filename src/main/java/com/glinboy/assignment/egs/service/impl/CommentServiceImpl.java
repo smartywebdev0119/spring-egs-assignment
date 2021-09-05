cb01fd792382490789d85e5244c3df85155a94a4
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-09-05 17:44:47 +0430

    👌 IMPROVE: Update Product rate by add/update comment

--- src/main/java/com/glinboy/assignment/egs/service/impl/CommentServiceImpl.java
+++ src/main/java/com/glinboy/assignment/egs/service/impl/CommentServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.glinboy.assignment.egs.model.Comment;
@@ -14,6 +15,7 @@ import com.glinboy.assignment.egs.model.RoleName;
 import com.glinboy.assignment.egs.model.User;
 import com.glinboy.assignment.egs.repository.CommentRepository;
 import com.glinboy.assignment.egs.service.CommentServiceApi;
+import com.glinboy.assignment.egs.service.ProductServiceApi;
 import com.glinboy.assignment.egs.service.dto.CommentDTO;
 import com.glinboy.assignment.egs.service.mapper.CommentMapper;
 import com.glinboy.assignment.egs.util.SecurityUtils;
@@ -24,30 +26,39 @@ public class CommentServiceImpl
 	implements CommentServiceApi {
 
 	private final EntityManager em;
+	private final ProductServiceApi productService;
 
 	public CommentServiceImpl(CommentRepository repository,
 			CommentMapper mapper,
-			EntityManager em) {
+			EntityManager em,
+			ProductServiceApi productService) {
 		super(repository, mapper);
 		this.em = em;
+		this.productService = productService;
 	}
 	
 	@Override
+	@Transactional
 	public CommentDTO save(CommentDTO commentDTO) {
 		Comment comment = this.mapper.toEntity(commentDTO);
 		comment.setUser(em.getReference(User.class, SecurityUtils.getCurrentUserPrincipal().getId()));
 		comment.setProduct(em.getReference(Product.class, commentDTO.getProductId()));
 		comment = this.repository.save(comment);
+		if(commentDTO.getId() == null) {
+			productService.updateUserRate(commentDTO.getProductId(), Short.valueOf("0"), commentDTO.getRate());
+		}
 		return this.mapper.toDto(comment);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Page<CommentDTO> getAll(Long productId, Pageable pageable) {
 		Page<Comment> page = this.repository.findAllByProductIdAndContentNotNull(productId, pageable);
 		return page.map(mapper::toDto);
 	}
 
 	@Override
+	@Transactional
 	public CommentDTO update(CommentDTO commentDTO) {
 		CommentDTO cmfound = this.findById(commentDTO.getId())
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Couldn't find any resource for: " + commentDTO.getId()));
@@ -58,16 +69,26 @@ public class CommentServiceImpl
 				!cmfound.getUserId().equals(SecurityUtils.getCurrentUserPrincipal().getId())) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Comment Does'nt blong to current user.");
 		}
-		return save(commentDTO);
+		CommentDTO saved = save(commentDTO);
+		productService.updateUserRate(commentDTO.getProductId(), cmfound.getRate(), commentDTO.getRate());
+		return saved;
 	}
 
 	@Override
+	@Transactional
 	public void deleteSingleById(Long productId, Long id) {
+		CommentDTO cmfound = this.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Couldn't find any resource for: " + id));
 		if(SecurityUtils.isCurrentUserInRole(RoleName.ROLE_ADMIN)) {
 			this.deleteSingleById(id);
 		} else {
-			this.repository.deleteByIdAndProductIdAndUserId(id, productId, SecurityUtils.getCurrentUserPrincipal().getId());
+			Long currentUserId = SecurityUtils.getCurrentUserPrincipal().getId();
+			if(!cmfound.getUserId().equals(currentUserId)) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You don't have right to delete this comment");
+			}
+			this.repository.deleteByIdAndProductIdAndUserId(id, productId, currentUserId);
 		}
+		productService.updateUserRate(productId, cmfound.getRate(), Short.valueOf("0"));
 	}
 
 }
