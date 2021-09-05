d9ab5888cec374945293282e662afb1aa3efbce3
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-09-06 00:47:10 +0430

    🚀 RELEASE: Version 1.0.1

--- src/main/java/com/glinboy/assignment/egs/web/rest/HomeController.java
+++ src/main/java/com/glinboy/assignment/egs/web/rest/HomeController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
