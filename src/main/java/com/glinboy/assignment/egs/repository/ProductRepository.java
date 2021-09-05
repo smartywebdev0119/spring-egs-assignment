5be8ccadcb3854456f38979ea061ae58abe65a41
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-09-05 17:44:09 +0430

    👌 IMPROVE: Handle rate update functionality in Product service

--- src/main/java/com/glinboy/assignment/egs/repository/ProductRepository.java
+++ src/main/java/com/glinboy/assignment/egs/repository/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.glinboy.assignment.egs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.glinboy.assignment.egs.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	@Modifying
+	@Query(value =  "UPDATE Product p SET p.rate = ( p.rate - ?2 + ?3 ) WHERE p.id = ?1",
+		nativeQuery = true)
+	void updateUserRateByProductId(Long productId, Short oldRate, Short newRate);
 
 }
