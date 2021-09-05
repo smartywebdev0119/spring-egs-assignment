a9a33d1d5a410d388ff3242c54af530dc357dbf3
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-09-05 12:42:10 +0430

    📦 NEW: Added User/Role DTO

---
+++ src/main/java/com/glinboy/assignment/egs/service/dto/UserDTO.java
@@ -0,0 +1,31 @@
+package com.glinboy.assignment.egs.service.dto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=true)
+public class UserDTO extends BaseDTO {
+
+	@NotBlank
+	@Size(max = 40)
+	private String firstname;
+
+	@NotBlank
+	@Size(max = 40)
+	private String lastname;
+
+	@NotBlank
+	@Size(max = 100)
+	@Email
+	private String email;
+
+	private Set<RoleDTO> roles = new HashSet<>();
+}
